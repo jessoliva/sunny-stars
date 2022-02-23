3db0cb588b1e7027e141a06e7ce85e8d3b3247b6
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-02-23 14:57:48 -0600

    HTML: added containers for search section and drop down for previously searched cities

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+// Weather Dashboard Challenge 6 PsuedoCode
+
+// 1. Explore Open Weather API and Documentation\
+// 2. Figure out to get an API-Key for Open Weather API (which may involve making an account)
+// 3. Create a search box
+    // -HTML markup, css
+    // API call to run onClick of the search button 
+    // Pass the user input into the url of the API call appropriately
+// 4. Display the queried city's information on the dashboard for the user to see including forecast
+// 5. When we display the city's UV index, we want to display it in a color that matches the number 
+// 6. Store the user's search history in local storage
+//    -get the user's search history and display it in the search dropdown 
+//    -when the user clicks the city from the dropdown, pass the city into the api call to get and display the info from that city again
+
+// HINT: Abstract API call to a function with a city as the parameter to be passed into the url 
+
